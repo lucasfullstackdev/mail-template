e56494c0d05afe15427f6ea1afcd0fcc9bd085eb
lucasfullstackdev <lucas.fullstack.dev@gmail.com>
2020-09-26 09:00:15 -0300

    npm install | npm i bootstrap | configuracao do webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+
+mix
+    .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css')
+    .copy('node_modules/bootstrap/dist/css/bootstrap.min.css.map', 'public/css/bootstrap.min.css.map')
+    .version();
+    
